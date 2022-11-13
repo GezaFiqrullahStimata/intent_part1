45ddf992abbeb5d37e8853f5ee34eda7cc0ca86d
Geza Fiqrullah Alamsyah Vitsu Budi Wardana <gezayu@gmail.com>
2022-11-13 20:29:06 +0700

    add dial number function

--- app/src/main/java/com/example/piranti/MainActivity.kt
+++ app/src/main/java/com/example/piranti/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.piranti
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -9,6 +10,7 @@ import android.widget.Button
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var btnMoveActivity : Button
     private lateinit var btnMoveActivityData : Button
+    private lateinit var btnDialNumber : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +21,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         btnMoveActivityData = findViewById(R.id.btn_move_activity_data)
         btnMoveActivityData.setOnClickListener(this)
+
+        btnDialNumber = findViewById(R.id.btn_dial_number)
+        btnDialNumber.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
@@ -34,6 +39,11 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                     bundle.putString("Nama","Geza")
                     bundle.putString("Alamat","Karangploso")
                     intent.putExtras(bundle)
+                    startActivity(intent)
+                }
+                R.id.btn_dial_number -> run{
+                    val dialNumber = "0859186074691"
+                    val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + dialNumber))
                     startActivity(intent)
                 }
             }
